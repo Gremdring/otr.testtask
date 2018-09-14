d7b9e7011c1bd4556484efb066c1e517f1ff98a7
gremdring <gremdring@gmail.com>
2018-09-14 12:25:45 +0500

    Задача 2. Запуск приложения.
    В приложении добавлен вызов нужного класса. Функционал работает
    корректно

--- PhoneBook/src/main/java/otr/testtask/PhoneBook/App.java
+++ PhoneBook/src/main/java/otr/testtask/PhoneBook/App.java
@@ -1,13 +1,17 @@
 package otr.testtask.PhoneBook;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	 Scanner scan = new Scanner(System.in);
+    	 String s = scan.nextLine();
+    	 
+    	 PhoneBook p = new PhoneBook();
+    	 p.PrintThePhoneList(s);
+    	 
     }
 }
