d16dd64bf3fe93502b97f9775081ccb96d22228a
gremdring <gremdring@gmail.com>
2018-09-14 12:24:19 +0500

    Задача 2. Дописывание класса
    Все методы доделаны. Все тесты проходятся.

--- PhoneBook/src/main/java/otr/testtask/PhoneBook/PhoneBook.java
+++ PhoneBook/src/main/java/otr/testtask/PhoneBook/PhoneBook.java
@@ -5,20 +5,48 @@ package otr.testtask.PhoneBook;
 
 import java.util.*;
 
+import javax.xml.stream.events.StartDocument;
+
 public class PhoneBook {
 	
 	private HashMap<String,ArrayList<String>> pBook = new HashMap<String,ArrayList<String>>(3,3);
 	
 	PhoneBook(){
-		//TODO: заполнение HashMap значениями из задания
+		
+		ArrayList<String> Ivanov = new  ArrayList<String>(0);
+		Ivanov.add("+8 800 2000 500");
+		Ivanov.add("+8 800 200 600");
+		pBook.put("Иванов И.И.", Ivanov);
+		
+		//можно для заполнения карты использовать одну переменную ArrayList
+		//Но так наглядней. И в данной задаче память - не проблема.
+		
+		ArrayList<String> Petrov = new  ArrayList<String>(0);
+		Petrov.add("+8 800 2000 700");
+		pBook.put("Петров П.П.", Petrov);
+		
+		
+		ArrayList<String> Sidorov = new  ArrayList<String>(0);
+		Sidorov.add("+8 800 2000 800");
+		Sidorov.add("+8 800 2000 900");
+		Sidorov.add("+8 800 2000 000");
+		pBook.put("Сидоров С.С.", Sidorov);	
+		
 	}
 	
 
 	ArrayList<String> GetTheList(String inp){
-		if (false) {
-			//TODO: поиск по HashMap и выдача нужного массива
-			return new ArrayList<String>(0);
+		if (pBook.containsKey(inp)) {
+			ArrayList<String> pList =  pBook.get(inp);
+			ArrayList<String> res 	=  new ArrayList<String>(0);
+			
+			for(int i = 0 ; i < pList.size(); i++) {
+				res.add( (i+1) +". " + pList.get(i)); 
+			}
+			
+			return res;
 		}
+	
 		else {
 			return ThereIsNoSuchMan();
 		}
@@ -26,7 +54,11 @@ public class PhoneBook {
 	
 	void PrintThePhoneList(String inp) {
 		ArrayList<String> res = GetTheList(inp);
-		//TODO: вывод на экран результата
+		Iterator<String> iter = res.iterator();
+		
+		while(iter.hasNext()) {
+			System.out.println(iter.next()); 
+		}
 		
 	}
 	
